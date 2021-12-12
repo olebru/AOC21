9e0b100c77f3d6db7eb99631021027025cf5d36c
Ole I Bruvik <oleib@me.com>
2021-12-13 00:52:42 +0100

    .

---
+++ 6/Program.cs
@@ -0,0 +1,38 @@
+﻿var input = System.IO.File.ReadAllLines(".\\Input.txt");
+
+var prev = new List<int>();
+var fishList = new List<int>();
+
+var currentData = new Int64[9];
+var nextData = new Int64[9];
+
+
+foreach (var item in input[0].Split(","))
+{
+    currentData[Convert.ToInt64(item)] = currentData[Convert.ToInt64(item)] + 1;
+}
+
+int days = 256;
+
+for (int i = 1; i <= days; i++)
+{
+    nextData = new Int64[9];
+    nextData[0] = currentData[1];
+    nextData[1] = currentData[2];
+    nextData[2] = currentData[3];
+    nextData[3] = currentData[4];
+    nextData[4] = currentData[5];
+    nextData[5] = currentData[6];
+    nextData[6] = currentData[7] + currentData[0];
+    nextData[7] = currentData[8];
+    nextData[8] = currentData[0];
+
+    currentData = nextData;
+
+    Console.WriteLine($"Day {i} {nextData.Sum()}");
+
+
+}
+
+Console.WriteLine($"Number of fish {currentData.Sum()}");
+
