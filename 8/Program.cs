c4c87c3875216c0ad734e6d9360c61fe922a91a0
Ole I Bruvik <oleib@me.com>
2021-12-13 12:53:45 +0100

    Day 8 p1

---
+++ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = System.IO.File.ReadAllLines(".\\Input.txt");
+
+var result = 0;
+foreach (var line in input)
+{
+    foreach (var digit in line.Split(" | ")[1].Split(" "))
+    {
+
+      switch (digit.Length)
+      {
+          case 2:
+          case 3:
+          case 4:
+          case 7:
+          result++;
+          break;
+          default:
+          break;
+      }   
+    }  
+}
+Console.WriteLine(result);
